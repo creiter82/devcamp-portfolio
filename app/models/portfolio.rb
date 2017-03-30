c31bb6f325406ca1f96be1bfb7d19c4b22dbe047
Cameron Reiter <reiter303@hotmail.com>
2017-03-30 15:18:15 -0600

    Implement concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,7 @@
 class Portfolio < ApplicationRecord
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  include Placeholder
   after_initialize :set_defaults
+  validates_presence_of :title, :body, :main_image, :thumb_image
 
   # Custom scopes => see portfolio controller
   def self.angular
@@ -12,7 +13,7 @@ class Portfolio < ApplicationRecord
   # the last example is a lambda
 
   def set_defaults
-    self.main_image ||= "http://placehold.it/600x400"
-    self.thumb_image ||= "http://placehold.it/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end

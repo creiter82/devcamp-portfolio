19c76b66d3cf635a4f8aa02edaf1cf21aa7f0a81
Cameron Reiter <reiter303@hotmail.com>
2017-03-30 15:33:26 -0600

    Integrate technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  has_many :technologies
   include Placeholder
   after_initialize :set_defaults
   validates_presence_of :title, :body, :main_image, :thumb_image

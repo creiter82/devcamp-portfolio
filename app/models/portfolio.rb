a11dca862dea80835d21a2d9231dccd20a387fd1
Cameron Reiter <reiter303@hotmail.com>
2017-03-30 14:00:16 -0600

    Implement custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,12 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  # Custom scopes => see portfolio controller
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
+  scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
+  # above is two different ways of creating custom scopes
+  # the last example is a lambda
 end

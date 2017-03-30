e9319e10e49ffe85f8d5c6b590cb76060ed1fd9a
Cameron Reiter <reiter303@hotmail.com>
2017-03-30 14:20:05 -0600

    Implement image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,6 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+  after_initialize :set_defaults
 
   # Custom scopes => see portfolio controller
   def self.angular
@@ -9,4 +10,9 @@ class Portfolio < ApplicationRecord
   scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
   # above is two different ways of creating custom scopes
   # the last example is a lambda
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end

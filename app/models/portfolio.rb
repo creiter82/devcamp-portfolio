1cb7354a3d6cf269f38118d798f91ed22c194b6a
Cameron Reiter <reiter303@hotmail.com>
2017-05-02 16:50:55 -0600

    Implement initial file upload system via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,8 +3,10 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
   include Placeholder
-  after_initialize :set_defaults
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
 
   # Custom scopes => see portfolio controller
   def self.angular

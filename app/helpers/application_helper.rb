4ff0f5b17d06ff0cc42a9d3e7780bf4130e168f8
Cameron Reiter <reiter303@hotmail.com>
2017-05-04 14:03:46 -0600

    Refactor alerts to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,4 +60,28 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg, title = 'Cameronreiter.com'
+    js add_gritter(msg, title: title, sticky: false)
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
 end

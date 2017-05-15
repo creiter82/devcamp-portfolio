3729eb3d6456261ca1c5a6ee2892e4884876005d
Cameron Reiter <reiter303@hotmail.com>
2017-05-14 21:26:17 -0600

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
+User.create!(
+  email: "test@test.com",
+  name: "Admin User",
+  password: "password",
+  password_confirmation: "password",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  name: "Regular User",
+  password: "password",
+  password_confirmation: "password"
+)
+
+puts "1 Regular user created"
+
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
